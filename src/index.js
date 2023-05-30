54758d4525263c225c7af564e09cb297d543dc06
tomono-mitsunori <134925130+tomono-mitsunori@users.noreply.github.com>
2023-05-30 17:56:37 +0900

    Finish section1 Finish section1

--- src/index.js
+++ src/index.js
@@ -69,15 +69,153 @@
 // function func1(str) {
 //   return str;
 // }
-const func1 = function (str) {
-  return str;
-};
-console.log(func1("func1です"));
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
 
-// アロー関数
-const func2 = (str) => str;
-console.log(func2("func2です"));
+// // アロー関数
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
 
-const func3 = (num1, num2) => num1 + num2;
+// const func3 = (num1, num2) => num1 + num2;
 
-console.log(func3(10, 20));
+// console.log(func3(10, 20));
+
+/**
+ * 分割代入
+ */
+// const myProfile = {
+//   name: "じゃけぇ",
+//   age: 28,
+// };
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です。';
+// console.log(message1);
+// const { name, age } = myProfile;
+// const message2 = '名前は${name}です。年齢は${age}歳です。';
+// console.log(message2);
+
+// const myProfile = ['じゃけぇ', 28];
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+
+// console.log(message3);
+
+// const [name, age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// console.log(message4);
+
+/**
+ * デフォルト値、引数など
+ */
+// const sayHello = (name) => console.log(`こんにちは！${name}さん！`);
+// sayHello("じゃけぇ");
+
+/**
+ * スプレッド構文 ...
+ */
+// 配列の展開
+// const arr1 = [1, 2];
+// //console.log(arr1);
+// //console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+//  const arr6 = [...arr4];
+//   arr6[0] = 100;
+//  console.log(arr6);
+//  console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// console.log(arr8);
+// arr8[0] = 100;
+// console.log(arr4);
+
+/**
+ * maplやfilterを使った配列の処理
+ */
+
+const nameArr = ["田中", "山田", "じゃけぇ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+//nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゃけぇ") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// // ある条件 ? 条件がtrueのとき　：　条件がfalseのとき
+// const val1 = 1 < 0 ? `trueです` : ` falseです`;
+// console.log(val1);
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num == 'number' ? num.toLocaleString() : '数値を入力して下さい';
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています!!' : '許容範囲内です';
+// }
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = false;
+
+// if (flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+
+// if (flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "なにか設定されました";
+console.log(fee2);
